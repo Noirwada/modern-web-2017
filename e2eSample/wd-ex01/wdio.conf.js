cbfddae1dada603ee432f91f4547c68a52d4f733
dmoon <dmoon.t@gmail.com>
2017-08-10 15:57:02 +0800

    update jenkinfile

--- e2eSample/wd-ex01/wdio.conf.js
+++ e2eSample/wd-ex01/wdio.conf.js
@@ -72,7 +72,7 @@ exports.config = {
     //
     // Set a base URL in order to shorten url command calls. If your url parameter starts
     // with "/", then the base url gets prepended.
-    host: 'localhost', //'selenium',
+    host: 'selenium', //'selenium',
     baseUrl: 'http://localhost',
     //
     // Default timeout for all waitFor* commands.
