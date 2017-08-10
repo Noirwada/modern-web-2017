be940199ef7461a5d3ca73fa7f66b7e90a39a98c
dmoon <dmoon.t@gmail.com>
2017-08-10 17:26:35 +0800

    my test

--- e2eSample/wd-ex01/test/specs/test.js
+++ e2eSample/wd-ex01/test/specs/test.js
@@ -11,10 +11,16 @@ describe('第一個前端測試程式', function () {
   it('登入失敗', function () {
     browser.url('http://demo.keystonejs.com/keystone/signin');
     // 輸入帳號
+    $('[name="email"]').setValue('aaa@bbb.com');
     // 輸入錯誤密碼
+    $('[name="password"]').setValue('ddd');
     // 按送出按鈕
+    $('[type="submit"]').click();
     // 檢查是否出現警告訊息
+    $('[data-alert-type="danger"]').waitForExist();
     // 警告訊息的文字內容，是否如預期
+    let s = $('[data-alert-type="danger"]').getText();
+    assert.equal(s, 'The email and password you entered are not valid.');
   });
 
   it('登入成功', function () {
